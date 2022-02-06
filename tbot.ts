dd8c1a26702ef30ac84f286681c82f2d5aa279dd
bnnlukas <bonn_lukas@gmx.de>
2022-02-07 00:42:35 +0100

    Update tbot.ts

--- tbot.ts
+++ tbot.ts
@@ -3,8 +3,9 @@ import "https://deno.land/x/dot_env@0.2.0/load.ts"
 import { CryptoPriceApi } from "./crypto_price_api.ts";
 let test;
 let output;
+let fiat;
 
-const TOKEN = "5174997578:AAEWhS1Kc7RkKAlevKkNpEJzsTEbu7S9eZk";
+const TOKEN = '5174997578:AAEWhS1Kc7RkKAlevKkNpEJzsTEbu7S9eZk';
 if (!TOKEN) throw new Error("Bot token is not provided");
 const bot = new TelegramBot(TOKEN);
 
@@ -12,10 +13,11 @@ const bot = new TelegramBot(TOKEN);
 bot.on(UpdateType.Message, async (message: any) => {
 
     const text : string = message.message.text || "I can't hear you";
+    fiat = text.split('-')[1];
     test = new CryptoPriceApi();
     output = await test.getSpotPriceTelegram(text);    
     console.log(output);
-    await bot.sendMessage({ chat_id: message.message.chat.id, text: `Der Preis liegt bei ${output}` })
+    await bot.sendMessage({ chat_id: message.message.chat.id, text: `Der Preis liegt bei ${output} ${fiat}` })
 
 });
 
