86f1bb3319576cff88ce6b1095163434a91408b8
bnnlukas <bonn_lukas@gmx.de>
2022-02-07 09:00:50 +0100

    Update tbot.ts

--- tbot.ts
+++ tbot.ts
@@ -4,6 +4,7 @@ import { CryptoPriceApi } from "./crypto_price_api.ts";
 let test;
 let output;
 let fiat;
+let crypto;
 
 const TOKEN = '5174997578:AAEWhS1Kc7RkKAlevKkNpEJzsTEbu7S9eZk';
 if (!TOKEN) throw new Error("Bot token is not provided");
@@ -14,11 +15,17 @@ bot.on(UpdateType.Message, async (message: any) => {
 
     const text : string = message.message.text || "I can't hear you";
     fiat = text.split('-')[1];
+    crypto = text.split('-')[0];
     test = new CryptoPriceApi();
-    output = await test.getSpotPriceTelegram(text);    
-    console.log(output);
-    await bot.sendMessage({ chat_id: message.message.chat.id, text: `Der Preis liegt bei ${output} ${fiat}` })
 
+    try{
+        output = await test.getSpotPriceTelegram(text)
+        await bot.sendMessage({ chat_id: message.message.chat.id, text: `Der ${crypto} Preis liegt bei ${output} ${fiat}` })
+    }
+    catch(error){
+        await bot.sendMessage({ chat_id: message.message.chat.id, text: `Dieses Paar ist bei Coinbase nicht gelistet. Geben Sie ein gültiges Paar ein. Alle gültigen Paare finden Sie auf der folgenden Website: https://www.coinbase.com/de/price` })
+
+    }
 });
 
 bot.run({
